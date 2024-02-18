5cc6d600216e02972f871f2f48025146f35c46d3
Alex Ato <alexato0731@gmail.com>
2024-02-18 10:52:42 -0800

    adding text to string res file

--- app/src/main/java/com/example/fitjournal/components/cards/subcomponents/CardSeeDetailsText.kt
+++ app/src/main/java/com/example/fitjournal/components/cards/subcomponents/CardSeeDetailsText.kt
@@ -4,13 +4,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import com.example.fitjournal.R
 
 @Composable
 fun CardSeeDetailsText(modifier: Modifier = Modifier){
     Text(
-        text = "Click to view/edit workout",
+        text = stringResource(id = R.string.text_view_workout_details),
         style = MaterialTheme.typography.bodySmall,
         color = MaterialTheme.colorScheme.onPrimary,
         modifier = modifier,
