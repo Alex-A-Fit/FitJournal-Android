b72b7240ecc67f9725c013f4d33da29bda312686
Alex-A-Fit <alexato0731@gmail.com>
2024-04-15 21:59:04 -0700

    Adding realm db to codebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
+    id("io.realm.kotlin")
 }
 
 android {
@@ -99,4 +100,9 @@ dependencies {
 
     /* Lottie Animation */
     implementation("com.airbnb.android:lottie-compose:6.1.0")
+
+    /* Realm Db */
+    implementation("io.realm.kotlin:library-base:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // If using coroutines with the SDK
+
 }
