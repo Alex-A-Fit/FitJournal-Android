c3c89d6bd0a3100489e5f68ebe0570a1b877a6c2
Alex-A-Fit <alexato0731@gmail.com>
2024-05-21 12:29:02 -0700

    Clean up EditWorkoutSetDialog Keep error strings with similar syntax cohesive

--- app/src/main/java/com/example/fitjournal/core/presentation/commoncomponents/dialogs/EditWorkoutSetDialog.kt
+++ app/src/main/java/com/example/fitjournal/core/presentation/commoncomponents/dialogs/EditWorkoutSetDialog.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -64,7 +63,7 @@ fun EditWorkoutSetDialog(
         onDismissRequest = { onDismissRequest() },
         properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth(.95f)
@@ -76,113 +75,118 @@ fun EditWorkoutSetDialog(
                     )
                 )
         ) {
-            Column {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            color = MaterialTheme.colorScheme.primary,
-                            shape = RoundedCornerShape(
-                                topStart = Spacing.spacing16,
-                                topEnd = Spacing.spacing16
-                            )
-                        )
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(
-                                horizontal = Spacing.spacing8,
-                                vertical = Spacing.spacing8
-                            )
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.title_edit_set),
-                            modifier = Modifier
-                                .weight(1f)
-                                .fillMaxWidth()
-                                .offset(x = Spacing.spacing16),
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            style = MaterialTheme.typography.headlineSmall,
-                            textAlign = TextAlign.Center
-                        )
-                        Spacer(modifier = Modifier.width(Spacing.spacing16))
-                        Icon(
-                            painter = painterResource(id = R.drawable.icon_circular_cancel),
-                            contentDescription = stringResource(id = R.string.content_desc_close_dialog_icon),
-                            modifier = Modifier
-                                .size(Spacing.spacing32)
-                                .clickable {
-                                    onDismissRequest()
-                                },
-                            tint = MaterialTheme.colorScheme.onPrimary
-                        )
+            TitleAndCancelButton(onDismissRequest = onDismissRequest)
+            Spacer(modifier = Modifier.height(Spacing.spacing16))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = Spacing.spacing16
+                    )
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                EditWorkoutSetProperties(
+                    workoutTypeDialog = workoutTypeDialog,
+                    isWorkoutValid = { isValidWorkout, workout ->
+                        isWorkoutValid = isValidWorkout
+                        if (workout != null) {
+                            newUpdatedWorkout = workout
+                        }
                     }
-                }
+                )
                 Spacer(modifier = Modifier.height(Spacing.spacing16))
-                Column(
+                ClearButton(
                     modifier = Modifier
+                        .fillMaxHeight()
                         .fillMaxWidth()
                         .padding(
-                            horizontal = Spacing.spacing16
-                        )
-                        .verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    EditWorkoutSetProperties(
-                        workoutTypeDialog = workoutTypeDialog,
-                        isWorkoutValid = { isValidWorkout, workout ->
-                            isWorkoutValid = isValidWorkout
-                            if (workout != null) {
-                                newUpdatedWorkout = workout
-                            }
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(Spacing.spacing16))
-                    ClearButton(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .fillMaxWidth()
-                            .padding(
-                                horizontal = Spacing.spacing32,
-                                vertical = Spacing.spacing16
-                            ),
-                        textModifier = Modifier
-                            .padding(
-                                horizontal = Spacing.spacing32,
-                                vertical = Spacing.spacing4
-                            ),
-                        onClick = onDismissRequest,
-                        text = stringResource(id = R.string.button_cancel_edit)
-                    )
-                    Spacer(modifier = Modifier.height(Spacing.spacing8))
-                    SaveButton(
-                        text = stringResource(id = R.string.button_update_workout),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(
-                                horizontal = Spacing.spacing32,
-                                vertical = Spacing.spacing16
-                            ),
-                        textModifier = Modifier.padding(
+                            horizontal = Spacing.spacing32,
+                            vertical = Spacing.spacing16
+                        ),
+                    textModifier = Modifier
+                        .padding(
                             horizontal = Spacing.spacing32,
                             vertical = Spacing.spacing4
                         ),
-                        onClick = {
-                            onSaveWorkoutPressed(newUpdatedWorkout)
-                        },
-                        buttonColor = ButtonColors(
-                            containerColor = MaterialTheme.colorScheme.primary,
-                            contentColor = MaterialTheme.colorScheme.onPrimary,
-                            disabledContainerColor = DisabledBackgroundGray,
-                            disabledContentColor = MediumGray
+                    onClick = onDismissRequest,
+                    text = stringResource(id = R.string.button_cancel_edit)
+                )
+                Spacer(modifier = Modifier.height(Spacing.spacing8))
+                SaveButton(
+                    text = stringResource(id = R.string.button_update_workout),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = Spacing.spacing32,
+                            vertical = Spacing.spacing16
                         ),
-                        isEnabled = isWorkoutValid
-                    )
-                }
+                    textModifier = Modifier.padding(
+                        horizontal = Spacing.spacing32,
+                        vertical = Spacing.spacing4
+                    ),
+                    onClick = {
+                        onSaveWorkoutPressed(newUpdatedWorkout)
+                    },
+                    buttonColor = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                        disabledContainerColor = DisabledBackgroundGray,
+                        disabledContentColor = MediumGray
+                    ),
+                    isEnabled = isWorkoutValid
+                )
             }
+        }
+    }
+}
+
+@Composable
+private fun TitleAndCancelButton(
+    onDismissRequest: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(
+                    topStart = Spacing.spacing16,
+                    topEnd = Spacing.spacing16
+                )
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    horizontal = Spacing.spacing8,
+                    vertical = Spacing.spacing8
+                )
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(id = R.string.title_edit_set),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .offset(x = Spacing.spacing16),
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.headlineSmall,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.width(Spacing.spacing16))
+            Icon(
+                painter = painterResource(id = R.drawable.icon_circular_cancel),
+                contentDescription = stringResource(id = R.string.content_desc_close_dialog_icon),
+                modifier = Modifier
+                    .size(Spacing.spacing32)
+                    .clickable {
+                        onDismissRequest()
+                    },
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
         }
     }
 }
